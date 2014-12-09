603ec12046baf1f827bd0067ccf3e9fffb5159d7
Morteza Shahriari Nia <m.shahriarinia@gmail.com>
2014-12-09 14:38:55 -0500

    debug

--- config/config.js
+++ config/config.js
@@ -9,7 +9,8 @@ if (typeof process.env.NODE_ENV === 'undefined') {
 	console.error(chalk.red('NODE_ENV is not defined! Using "development" as environment (among {development,production,test})'));
 	process.env.NODE_ENV = 'development';
 } else {
-	var env_path = './env/' + process.env.NODE_ENV +'.js';
+	var env_path = './config/env/' + process.env.NODE_ENV +'.js';
+	console.log(env_path);
 	if(!fs.existsSync(env_path)){
 		console.error(chalk.red('No configuration file found for "' + process.env.NODE_ENV + '" environment. Using "development" as environment (among {development,production,test})'));
 		process.env.NODE_ENV = 'development';
