0d61fe023a5c27b38dad9c1cbc332c94b0a852da
Morteza Shahriari Nia <m.shahriarinia@gmail.com>
2014-12-09 14:22:49 -0500

    Added remote mongo database

--- config/env/production.js
+++ config/env/production.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-	db: '',
+	db: '  mongodb://awesome.mail.ly:test12345678@ds063140.mongolab.com:63140/books',
 	app: {
-		title: 'Book Free Shipping Buy/Sell'
+		title: 'Free Shipping Used Books(TM)'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
@@ -16,12 +16,13 @@ module.exports = {
 		callbackURL: '/auth/twitter/callback'
 	},
 	mailer: {
-		from: process.env.MAILER_FROM || 'MAILER_FROM',
+		from: process.env.MAILER_FROM || 'awesome.mail.ly@gmail.com',
+		admins: [process.env.MAILER_FROM || 'awesome.mail.ly@gmail.com', 'beratali@yahoo.com', 'a.m.farrahi@gmail.com' ],
 		options: {
-			service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
+			service: process.env.MAILER_SERVICE_PROVIDER || 'gmail.com',
 			auth: {
-				user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
-				pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+				user: process.env.MAILER_EMAIL_ID || 'awesome.mail.ly@gmail.com',
+				pass: process.env.MAILER_PASSWORD || 'test12345678'
 			}
 		}
 	}
